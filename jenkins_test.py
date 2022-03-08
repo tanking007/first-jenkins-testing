502535f6157e37f760cdf7a3fc810a60c2a0618f
Vipesh Tanwar <vipesh.tanwar@aptportfolio.com>
2022-03-08 12:36:44 +0530

    First Commit Adding jenkins_testing.py file

---
+++ jenkins_test.py
@@ -0,0 +1,16 @@
+import os
+
+list1 = [8,9,3,6,1,10]
+list1.reverse()
+print("The Reversed list is", list1)
+
+list2 = [91,67,120,34,76,54,78,8756,64,345]
+list2.sort()
+print("The Sorted list is", list2)
+
+list3 = []
+list3 = list3.copy()
+print("List #3 =",list3)
+
+indexvalue=list2[2:6]
+print("The IndexValues are ", indexvalue)
